2e41e5ccf30b9eaed3905ffdbf858b562ada3e2a
Mauro Villagra <maurovillagra257@gmail.com>
2023-08-04 22:46:30 -0300

    Termine el Programa

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import { useState } from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap'
+import Formulario from "./componentes/Formulario";
 
 function App() {
 
   return (
     <>
-      
+    <Container>
+      <h1 className='my-5 text-center'>Portal de Noticias</h1>
+      <Formulario/>
+    </Container>
     </>
   )
 }
